231716af986ae5cc3e0dee05f784d4cfb39fffbf
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-05 15:29:56 +0600

    update food section

--- src/component/Food/Food.jsx
+++ src/component/Food/Food.jsx
@@ -10,27 +10,27 @@ const Food = () => {
             <div className='food-section'>
                 <div className='food-card'>
                     <img src={food1} alt="" />
-                    <h4>Hanny</h4>
+                    <h4>BlueBary Honey</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti ad necessitatibus ullam.</p>
-                    <h5>price:231 tk</h5>
+                    <h5>price:201 tk</h5>
                 </div>
                 <div className='food-card'>
                     <img src={food2} alt="" />
-                    <h4>Hanny</h4>
+                    <h4>Waildflower Honey</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti ad necessitatibus ullam.</p>
-                    <h5>price:231 tk</h5>
+                    <h5>price:121 tk</h5>
                 </div>
                 <div className='food-card'>
                     <img src={food3} alt="" />
-                    <h4>Hanny</h4>
+                    <h4>Queen Be Honey</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti ad necessitatibus ullam.</p>
-                    <h5>price:231 tk</h5>
+                    <h5>price:289 tk</h5>
                 </div>
                 <div className='food-card'>
                     <img src={food4} alt="" />
-                    <h4>Hanny</h4>
+                    <h4>Sage Honey</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti ad necessitatibus ullam.</p>
-                    <h5>price:231 tk</h5>
+                    <h5>price:181 tk</h5>
                 </div>
             </div>
         </div>
