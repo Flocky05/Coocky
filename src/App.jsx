716d1a8ff56958c6c656e62c3e490774250334bd
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-05 23:25:29 +0600

    added honey section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Body from './component/Body/Body'
 import Food from './component/Food/Food'
 import Header from './component/Header/Header'
+import Honey from './component/Honey/Honey'
 import Services from './component/Services/Services'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Body></Body>
         <Food></Food>
         <Services></Services>
+        <Honey></Honey>
       </div>
       
     </>
