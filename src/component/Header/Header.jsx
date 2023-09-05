3a5b4d2199ab9e81d0e22f415ea18560a944e08b
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-05 12:14:08 +0600

    added Coocky project

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <p>Hi bro</p>
+        </div>
+    );
+};
+
+export default Header;
