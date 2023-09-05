6a1adb3028e5ef0f3f372fd8031f9be1bf9fde70
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-05 12:22:57 +0600

    added header section

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+
 import './Header.css'
+import logo from '../../assets/images/favicons/favicons.png'
 const Header = () => {
     return (
-        <div>
-            <p>Hi bro</p>
-        </div>
+        <nav className='header'>
+            <div className='header-left'>
+                <img src={logo} alt="" />
+                <a href="/home">Coocky</a>
+            </div>
+            <div className='header-right'>
+                <a href="/Home">Order</a>
+                <a href="/Home">Blog</a>
+                <a href="/Home">About Us</a>
+                <a href="/Home">Login</a>
+            </div>
+        </nav>
     );
 };
 
