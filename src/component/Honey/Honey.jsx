bef3961ce9a70b9c855474d5bb74a00d27a035ad
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-05 23:37:12 +0600

    update honey section

--- src/component/Honey/Honey.jsx
+++ src/component/Honey/Honey.jsx
@@ -4,11 +4,14 @@ const Honey = () => {
     return (
         <div className="honey-container">
             <img src={hero} alt="" />
-            <div>
+            <div className="honey-body">
                 <h2>Living well begins <br /> with eating well</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum magnam nulla quidem expedita, debitis unde fugit adipisci veniam cupiditate accusamus.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laboriosam.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, debitis unde fugit adipisci veniam cupiditate accusamus laboriosam.</p>
+                <div>
                 <a href="">Explore story</a>
+
+                </div>
             </div>
         </div>
     );
