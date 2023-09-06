eec71b1a63be0be98dc3f63687f9397cc98e4ee0
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-06 14:37:06 +0600

    update body section

--- src/component/Body/Body.jsx
+++ src/component/Body/Body.jsx
@@ -9,7 +9,7 @@ const Body = () => {
                 <h2>Eat <span>Good</span> Think <span>Good</span></h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt harum dolor dolore sed quae cupiditate aperiam asperiores nisi et, voluptatem ipsa velit alias nobis, commodi facilis rem. Autem nihil maxime iste voluptate eligendi ea recusandae tenetur animi perspiciatis aliquam.</p>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam sapiente omnis quasi, fugit ab, vel quae, architecto quam hic ipsa adipisci quod autem illo aut laborum aperiam magni exercitationem ad possimus dolorum.</p>
-                <button>Learn More</button>
+                <a>Learn More</a>
             </div>
             <div>
                 <img src={chef} alt="" />
