7936d62ba3f7ed7cc7270e35fc948e2332e8bc25
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-11-30 14:24:16 +0600

    update footer section

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import img2 from "../../assets/images/food/f2.png"
 
 const Footer = () => {
     return (
-        <div className='footer-section'>
+        <div className='footer-section '>
             <div className='footer-body'>
                 <img src={img1} alt="" />
                 <img src={img2} alt="" />
